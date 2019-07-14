7cd00fd7ce87514e3d7d0cd799ea0deb6ec63421
Moritz Stüber <moritz.stueber@aut.uni-saarland.de>
2019-07-14 18:01:56 +0200

    Turn array of pd.Series into correctly shaped np.ndarray

--- worker/worker.py
+++ worker/worker.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python3
 # -*- coding: utf8 -*-
 
+import numpy as np
 import pandas as pd
 from loguru import logger
 
@@ -33,4 +34,22 @@ def timeseries_dict_to_pd_series(ts_dict):
 def prepare_bc_for_fmpy(ts, units=None):
     """Turn array of pd.Series into correctly shaped np.ndarray."""
 
-    pass
+    df = pd.DataFrame(ts[0])
+
+    for series in ts[1:]:
+        df = df.join(series)
+
+    # Ensure that seconds relative to offset are used as index
+    offset = df.index.min()
+    df['time_rel'] = df.index
+    df['time_rel'] = df['time_rel'].apply(lambda x: float((x - offset)/1000))
+    df.set_index('time_rel', inplace=True)
+    df.index.rename('time', inplace=True)
+
+    # Replace NaN with constant value
+    df.fillna(value=FILLNA, inplace=True)
+
+    # Transform into np.ndarray with correct dtypes
+    ndarray = np.array(df.to_records())
+
+    return ndarray
